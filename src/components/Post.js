e9616ee27f68abc2a0620663213378899d3b84bf
Marty <maruri2424@gmail.com>
2022-12-05 15:02:31 -0800

    fixed bug where likes dont update on post page

--- src/components/Post.js
+++ src/components/Post.js
@@ -3,23 +3,24 @@ import PostSidebar from "./PostSidebar";
 import { useParams } from "react-router-dom";
 import { getPost } from "../firebase";
 
-function Post({ refresh }) {
+function Post() {
   const { postID } = useParams();
   const [post, setPost] = useState('');
 
   useEffect(() => {
-    async function get() {
-      setPost(await getPost(postID));
-    }
     get();
   }, [postID]);
+
+  async function get() {
+    setPost(await getPost(postID));
+  }
   
   return (
     <div className="post-main">
       {post &&
         <div className="post-container">
           <img src={post.imageUrl} alt="post-img" className="post-img"/>
-          <PostSidebar post={post} refresh={refresh}/>
+          <PostSidebar post={post} refresh={get}/>
         </div>
       }
     </div>

ccffdb869ad89e191f71f3b3e5f46d44e11c9297
Marty <maruri2424@gmail.com>
2022-12-12 21:03:20 -0800

    enabled settings-- profile pic overlay to show on hover

--- src/components/ProfileInput.js
+++ src/components/ProfileInput.js
@@ -10,11 +10,21 @@ function ProfileInput({ user, refresh }) {
     }
   }
 
+  function handleMouseEnter() {
+    const overlay = document.getElementById('settings-profileOverlay');
+    overlay.style.opacity = '0.2';
+  }
+
+  function handleMouseLeave() {
+    const overlay = document.getElementById('settings-profileOverlay');
+    overlay.style.opacity = '0';
+  }
+
   return (
     <div className="settings-profilePic">
       <div className="settings-profileInputContainer">
         <label>
-          <input id="profilePicInput" type="file" accept="image/jpeg, image/png, image/jpg" className="settings-bannerInput" onInput={handleInput}/>
+          <input id="profilePicInput" type="file" accept="image/jpeg, image/png, image/jpg" className="settings-bannerInput" onInput={handleInput} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} />
         </label>
         <img className="settings-profileImg" alt="profile" src={user.profilePicUrl} />
       <div id="settings-profileOverlay"></div>

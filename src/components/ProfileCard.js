ecca494b9767d9ebfeab0a223a32731c6da3ee71
Marty <maruri2424@gmail.com>
2023-01-12 20:16:12 -0800

    added image blur on profile card

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -5,8 +5,11 @@ function ProfileCard ({ post }) {
   return (
     <div className="profile-card">
       <Link to={'/post/' + postID}>
+        <div className="profile-card-overlay">
+        </div>
         <img className="profile-card-img" alt="post img" src={imageUrl}/>
       </Link>
+      <img className="profile-card-blur" alt="post img" src={imageUrl}/>
     </div>
   )
 }

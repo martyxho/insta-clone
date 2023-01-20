f20d01010799ef46a5f5246e08e003ba3f886651
Marty <maruri2424@gmail.com>
2023-01-19 21:29:12 -0800

    styled profile sidebar

--- src/components/ProfileSidebar.js
+++ src/components/ProfileSidebar.js
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from "react";
-import { getUserFollowers, getUserFollows } from "../firebase";
 
 function ProfileSidebar ({ user, postCount }) {
 
   return (
     <div className="profile-sidebar">
-      <div>
-        {user.name}
+      <div className="profile-sidebar-userContainer">
+        <h2 className="profile-sidebar-username">{user.name}</h2>
+      </div>
+      <div className="profile-sidebar-infoContainer">
+        <div className="profile-sidebar-postsContainer">
+          <h3>{postCount}</h3>
+          <p className="profile-sidebar-posts">Posts</p>
+        </div>
+        <div className="profile-sidebar-userInfo">
+          <div className="profile-sidebar-followContainer">
+            <h3>{user.following.length}</h3> 
+            <p className="profile-sidebar-follow">Following</p>
+          </div>
+          <div className="profile-sidebar-followContainer">
+            <h3>{user.followers.length}</h3> 
+            <p className="profile-sidebar-follow">Followers</p>
+          </div>
+        </div>
       </div>
       <div>
-        {postCount} posts
-      </div>
-      <div>
-      {user.followingCount} following {user.followersCount} followers 
-      </div>
-      <div>
-        {user.bio}
+        <h3 className="profile-sidebar-bioHeader">Bio</h3>
+        <p className="profile-sidebar-bioText">{user.bio}</p>
       </div>
     </div>
   )

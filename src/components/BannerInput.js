bdcc83022077e5ac380144973d7201c42e90830a
Marty <maruri2424@gmail.com>
2023-01-20 20:18:59 -0800

    styled settings banner

--- src/components/BannerInput.js
+++ src/components/BannerInput.js
@@ -9,24 +9,19 @@ function BannerInput({ user, refresh }) {
       refresh();
     }
   }
-  function handleMouseEnterBanner() {
-    const overlay = document.getElementById('settings-bannerOverlay');
-    overlay.style.opacity = '0.3';
-  }
-
-  function handleMouseLeaveBanner() {
-    const overlay = document.getElementById('settings-bannerOverlay');
-    overlay.style.opacity = '0';
-  }
 
   return (
-    <div className="settings-banner">
-      <label>
-        <input id="bannerInput" type="file" accept="image/jpeg, image/png, image/jpg" className="settings-bannerInput" onInput={handleBanner} onMouseEnter={handleMouseEnterBanner} onMouseLeave={handleMouseLeaveBanner}/>
-      </label>
-      <img className="settings-bannerImg" alt="banner" src={user.bannerURL}/>
-      <div id="settings-bannerOverlay"></div>
-    </div>
+    <label className="settings-bannerOverlayContainer">
+      <input className="settings-fileInput" type="file" accept="image/jpeg, image/png, image/jpg" onInput={handleBanner}/>
+      <div className="settings-bannerContainer">
+        <div className="settings-banner">
+          <img className="settings-bannerImg" alt="banner" src={user.bannerURL}/>
+        </div>
+      </div>
+      <div className="settings-bannerOverlay">
+        <svg className="settings-bannerIcon" stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="44" d="M358.62 129.28L86.49 402.08 70 442l39.92-16.49 272.8-272.13-24.1-24.1zm54.45-54.44l-11.79 11.78 24.1 24.1 11.79-11.79a16.51 16.51 0 000-23.34l-.75-.75a16.51 16.51 0 00-23.35 0z"></path></svg>
+      </div>
+    </label>
   )
 }
 
